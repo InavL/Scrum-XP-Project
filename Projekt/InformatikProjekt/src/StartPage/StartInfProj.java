0cc93de7bf4b504dea17c460dd7e396a0dd7d531
InavL <ATB1998@gmail.com>
2019-01-30 12:18:50 +0100

    La till alla databasfiler samt uppdaterade projektet för det
    ✨ La till databasfiler
    ✨ La till att projektet använder sig av InfDB
    ✨ Slängde in grunden till databasen
    ✨ Skapade StartInfProj.java som projektet kommer startas ifrån
    🎨 Uppdaterade befintliga filer för att använda databasen

---
+++ Projekt/InformatikProjekt/src/StartPage/StartInfProj.java
@@ -0,0 +1,36 @@
+package StartPage;
+
+import java.io.File;
+import javax.swing.JOptionPane;
+import oru.inf.InfDB;
+import oru.inf.InfException;
+
+/**
+ *
+ * @author Alvin Johansson
+ */
+public class StartInfProj {
+    
+    private static InfDB idb;
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        // Denna fil körs för att starta programmet.
+        
+        try {
+            
+            String file = new File("IP.FDB").getAbsolutePath();
+            idb = new InfDB (file);
+            
+        } catch (InfException ettUndantag) {
+            JOptionPane.showMessageDialog(null, "Databasen kunde inte anslutas!");
+            System.out.println("Internt felmeddelande " + ettUndantag.getMessage());
+        }
+
+        new HomePage(idb).setVisible(true);
+    }
+    
+}
