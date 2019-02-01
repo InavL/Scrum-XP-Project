4dcdcee5a28400545e1e9983ba653949864d6cfb
rundboll <47029739+rundboll@users.noreply.github.com>
2019-02-01 09:09:40 +0100

    MethodClass

---
+++ Projekt/InformatikProjekt/src/StartPage/MethodClass.java
@@ -0,0 +1,20 @@
+
+package StartPage;
+
+import oru.inf.InfDB;
+
+
+public class MethodClass {
+    private InfDB idb;
+    
+    
+    
+    
+    public MethodClass(InfDB idb){
+        
+    
+        
+        
+    }
+    
+}
