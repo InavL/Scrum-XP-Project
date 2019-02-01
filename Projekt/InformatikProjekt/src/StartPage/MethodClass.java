a78987557d82472a837b9c7a857f81b492a08a4a
rundboll <47029739+rundboll@users.noreply.github.com>
2019-02-01 10:12:42 +0100

    Uppdatering

--- Projekt/InformatikProjekt/src/StartPage/MethodClass.java
+++ Projekt/InformatikProjekt/src/StartPage/MethodClass.java
@@ -1,6 +1,8 @@
 
 package StartPage;
 
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
 import oru.inf.InfDB;
 
 
@@ -15,6 +17,20 @@ public class MethodClass {
     
         
         
+    }
+    
+        public static ArrayList<String> kategori1(String katNamn, String katTabell)
+    {
+        try
+        {
+            String fraga = "select " + katNamn + " from " + katTabell;
+            ArrayList<String> allaKategorier1 = idb.fetchColumn(fraga);
+            return allaKategorier1;
+        }
+        catch(Exception ex)
+        {
+            JOptionPane.showMessageDialog(null, "Something went wrong.");
+        }
     }
     
 }
