aa6716ad068307a2755cce6df8508905758c127d
Lorvan <47029254+Lorvan@users.noreply.github.com>
2019-02-14 13:40:56 +0100

    SendEmail

--- Projekt/InformatikProjekt/src/StartPage/SendEmail.java
+++ Projekt/InformatikProjekt/src/StartPage/SendEmail.java
@@ -5,58 +5,68 @@
  */
 package StartPage;
 
+
+import java.util.Date;
+import java.util.Properties;
+import javax.mail.*;
+import javax.mail.internet.*;
+import javax.mail.internet.MimeMessage;
+
+
 /*import java.util.*;
 import javax.mail.*;
 import javax.mail.internet.*;
 import javax.activation.*;*/
-
 
 /**
  *
  * @author sadeerbedan
  */
 public class SendEmail {
+    
+    public static void main(String args []) {
+    
+    try{
+    String host = "smtp.gmail.com";
+    String user = "mailoruis@gmail.com";
+    String pass = "javagrupp1";
+    String to = "irmelilova@gmail.com";
+    String from = "mailoruis@mail.com";
+    String subject = "enkelt meddelande";
+    String messageText = "testar detta!!!";
+    boolean sessionDebug = false;
+      
+    Properties props = System.getProperties();
+    
+    props.put("mail.smtp.starttls.enable", "true");
+    props.put("mail.smtp.host", host);
+    props.put("mail.smtp.port", "587");
+    props.put("mail.smtp.auth", "true");
+    props.put("mail.smtp.starttls.required", "true" );
+     
+    java.security.Security.addProvider(new com.sun.net.ssl.internal.ssl.Provider());
+    
+    Session mailSession = Session.getDefaultInstance(props, null);
+    
+    mailSession.setDebug(sessionDebug);
+    Message msg = new MimeMessage(mailSession);
+    msg.setFrom(new InternetAddress(from));
+    InternetAddress[] address = {new InternetAddress(to)};
 
-  /* public static void main(String [] args) {    
-      // Recipient's email ID needs to be mentioned.
-      String to = "abcd@gmail.com";
 
-      // Sender's email ID needs to be mentioned
-      String from = "web@gmail.com";
-
-      // Assuming you are sending email from localhost
-      String host = "localhost";
-
-      // Get system properties
-      Properties properties = System.getProperties();
-
-      // Setup mail server
-      properties.setProperty("mail.smtp.host", host);
-
-      // Get the default Session object.
-      Session session = Session.getDefaultInstance(properties);
-
-      try {
-         // Create a default MimeMessage object.
-         MimeMessage message = new MimeMessage(session);
-
-         // Set From: header field of the header.
-         message.setFrom(new InternetAddress(from));
-
-         // Set To: header field of the header.
-         message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
-
-         // Set Subject: header field
-         message.setSubject("This is the Subject Line!");
-
-         // Now set the actual message
-         message.setText("This is actual message");
-
-         // Send message
-         Transport.send(message);
-         System.out.println("Sent message successfully....");
-      } catch (MessagingException mex) {
-         mex.printStackTrace();
-      }
-   }*/
-}
+    msg.setRecipients(Message.RecipientType.TO, address); 
+    msg.setSubject(subject);
+    msg.setSentDate(new Date());
+    msg.setText(messageText);
+    
+    Transport transport = mailSession.getTransport("smtp");
+    transport.connect(host, user, pass);
+    transport.sendMessage(msg, msg.getAllRecipients());
+    transport.close();
+    System.out.println("message send sucessfully");
+    } catch(Exception ex)
+       {
+        System.out.println(ex);
+        
+        }
+    }}
