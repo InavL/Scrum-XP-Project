c34cb954c65a6b6703ef06ada80a17ebc9eceb7b
malmgrenmalin <malmgren.malin@gmail.com>
2019-02-15 12:11:44 +0100

    m

--- Projekt/InformatikProjekt/src/StartPage/ShowThisCalender.java
+++ Projekt/InformatikProjekt/src/StartPage/ShowThisCalender.java
@@ -17,7 +17,7 @@ import oru.inf.InfDB;
  *
  * @author ellin
  */
-public class ShowMyCalender extends javax.swing.JInternalFrame {
+public class ShowThisCalender extends javax.swing.JInternalFrame {
     
     private static InfDB idb;
     private SimpleDateFormat sdf;
@@ -25,11 +25,10 @@ public class ShowMyCalender extends javax.swing.JInternalFrame {
     /**
      * Creates new form EditBlogInternalFrame
      */
-    public ShowMyCalender(InfDB idb) {
+    public ShowThisCalender(InfDB idb) {
         initComponents();
         this.idb = idb;
         sdf = new SimpleDateFormat("yyyy-MM-dd");
-       
     }
 
     /**
@@ -46,7 +45,7 @@ public class ShowMyCalender extends javax.swing.JInternalFrame {
         jLabel1 = new javax.swing.JLabel();
         lblText = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
-        ChooseDate = new com.toedter.calendar.JDateChooser();
+        chooseDate = new com.toedter.calendar.JDateChooser();
         jButton1 = new javax.swing.JButton();
         jScrollPane1 = new javax.swing.JScrollPane();
         textAreaShowMeeting = new javax.swing.JTextArea();
@@ -76,7 +75,7 @@ public class ShowMyCalender extends javax.swing.JInternalFrame {
 
         jLabel2.setText("Choose date");
 
-        ChooseDate.setDateFormatString("yyyy-MM-dd");
+        chooseDate.setDateFormatString("yyyy-MM-dd");
 
         jButton1.setText("OK");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
@@ -100,40 +99,41 @@ public class ShowMyCalender extends javax.swing.JInternalFrame {
                         .addGap(120, 120, 120)
                         .addComponent(lblText, javax.swing.GroupLayout.PREFERRED_SIZE, 343, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(98, 98, 98)
+                        .addGap(54, 54, 54)
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(ChooseDate, javax.swing.GroupLayout.PREFERRED_SIZE, 160, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(jLabel2)
+                            .addComponent(chooseDate, javax.swing.GroupLayout.PREFERRED_SIZE, 178, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(jButton1))))
-                .addGap(2, 2, 2)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 447, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(528, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 483, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(458, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(53, 53, 53)
+                .addComponent(jLabel2)
+                .addGap(29, 29, 29)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(85, 85, 85)
-                        .addComponent(jLabel2)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(ChooseDate, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(48, 48, 48)
+                        .addComponent(chooseDate, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(40, 40, 40)
                         .addComponent(jButton1)
-                        .addGap(90, 90, 90)
+                        .addGap(105, 105, 105)
                         .addComponent(lblText, javax.swing.GroupLayout.PREFERRED_SIZE, 26, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(96, 96, 96)
-                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 470, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap(184, Short.MAX_VALUE))
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 476, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(176, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -144,47 +144,44 @@ public class ShowMyCalender extends javax.swing.JInternalFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        Date dateStartDatum = ChooseDate.getDate();
+        try{
+        Date dateStartDatum = chooseDate.getDate();
         String choosenDate = sdf.format(dateStartDatum);
         
-        try{
-            // Hämtar möten som den inloggade personen har på valt datum
-            ArrayList<HashMap<String, String>> resultat = idb.fetchRows("SELECT SLUT_TID, START_TID, TYP_AV_MOTE FROM MOTEN"
+        LoggedUser loggedIn = new LoggedUser();
+        int personID = loggedIn.getID();
+       
+            ArrayList<HashMap<String, String>> resultat = idb.fetchRows("SELECT TYP_AV_MOTE, START_TID, SLUT_TID FROM MOTEN"
             + " join PERSONER_DELTAR on MOTEN.MID = PERSONER_DELTAR.MID"
             + " join PERSONER on PERSONER_DELTAR.ID = PERSONER.ID"
-            + " where DATEOFMEETING = \'" + choosenDate + "\'");
+            + " where DATEOFMEETING = \'" + choosenDate + " and PERSONER.ID = '" + personID + "'");
        
             textAreaShowMeeting.setText("");
-            //Loopar genom ArrayListen och hämtar sluttid, starttid och datumen för varje möte inloggad person har
                     for (int i = 0; i < resultat.size(); i++) {
-                        String sluttiden = resultat.get(i).get("SLUT_TID");
-                        String starttiden = resultat.get(i).get("START_TID");
                         String typAvMote = resultat.get(i).get("TYP_AV_MOTE");
+                        String starttiden = resultat.get(i).get("START_TID");
+                        String sluttiden = resultat.get(i).get("SLUT_TID");
                     
-                        textAreaShowMeeting.setText(sluttiden + " " + starttiden + " " + typAvMote + "\n");
+                        textAreaShowMeeting.setText(typAvMote + " " + starttiden + " " + sluttiden + "\n");
                        }
         }
         catch (Exception e){
                 JOptionPane.showMessageDialog(null, "Något gick fel!");
                 System.out.println("Internt felmeddelande"+e.getMessage());  
             }
-
     }//GEN-LAST:event_jButton1ActionPerformed
 
-     
+  
+
     
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private com.toedter.calendar.JDateChooser ChooseDate;
+    private com.toedter.calendar.JDateChooser chooseDate;
     private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
-    private javax.swing.JLabel jLabel4;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
-    private javax.swing.JPanel jPanel3;
-    private javax.swing.JPanel jPanel4;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JLabel lblText;
     private javax.swing.JTextArea textAreaShowMeeting;
